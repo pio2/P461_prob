9182052f845c3bfd9aad1e751605f620a9144b06
ykoni <konishi01@gmail.com>
2024-03-16 11:57:38 +0900

    add extend class

---
+++ src/main/java/org/example/AccountExtend.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class AccountExtend  extends Account{
+    String addressData;
+
+    public AccountExtend(String owner, int zandaka) {
+        super(owner, zandaka);
+    }
+}
