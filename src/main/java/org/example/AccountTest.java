61f9c33bd9a6c3aae09828928ff6b588e0264964
ykoni <konishi01@gmail.com>
2024-03-16 11:30:52 +0900

    add code transfer

--- src/main/java/org/example/AccountTest.java
+++ src/main/java/org/example/AccountTest.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.junit.jupiter.api.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AccountTest {
@@ -11,6 +10,13 @@ public class AccountTest {
         assertEquals(30000, a.zandaka);
     }
     @Test public void transfer() {
+        Account b = new Account("ミナト", 30000);
+        Account c = new Account("yamada", 100000);
+        b.transfer(c, 5000);
+        boolean ans = (b.zandaka == (30000 - 5000));
+        boolean ans2 = (c.zandaka == (100000 + 5000));
+        System.out.println(ans);
+        System.out.println(ans2);
 
     }
 }
