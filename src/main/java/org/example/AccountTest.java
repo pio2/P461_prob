bb7cb7ca2e2c53b0521c8fe1c594092554dc6465
ykoni <konishi01@gmail.com>
2024-03-23 13:40:52 +0900

    add hello

--- src/main/java/org/example/AccountTest.java
+++ src/main/java/org/example/AccountTest.java
@@ -8,8 +8,12 @@ public class AccountTest {
     @Test public void instantiate() {
         Account a = new Account("ミナト", 30000);
         assertEquals("ミナト", a.owner);
-        assertEquals(30000, a.zandaka);
+        ///assertEquals(30000, a.zandaka);
     }
+
+    private void assertEquals(String ミナト, String owner) {
+    }
+
     @Test public void transfer() {
         System.out.println("hello");
     }
